e6c910783f2b35dcd2a086ea52ddcbabf6bb8065
QAEng2827 <inna.nakopyuk@gmail.com>
2021-05-31 12:55:04 +0300

    innitial commit

---
+++ src/main/java/prog/kyiv/com/App.java
@@ -0,0 +1,13 @@
+package prog.kyiv.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
